66f5512660e3cd1fa8968e66b8f3ca3572f72c27
Dragooon <mail@dragooon.net>
2015-06-04 20:25:03 +0530

    Update ClientInterface

--- src/Dragooon/Hawk/Client/ClientInterface.php
+++ src/Dragooon/Hawk/Client/ClientInterface.php
@@ -18,7 +18,7 @@ interface ClientInterface
     /**
      * @param CredentialsInterface $credentials
      * @param Request $request
-     * @param $headerObjectOrString
+     * @param mixed $headerObjectOrString Response header
      * @param array $options
      * @return mixed
      */
@@ -28,4 +28,28 @@ interface ClientInterface
         $headerObjectOrString,
         array $options = array()
     );
+
+    /**
+     * @param CredentialsInterface $credentials
+     * @param $uri
+     * @param $ttlSec
+     * @param array $options
+     * @return mixed
+     */
+    public function createBewit(CredentialsInterface $credentials, $uri, $ttlSec, array $options = array());
+
+
+    /**
+     * Generate an authorization string for a message
+     *
+     * @param CredentialsInterface $credentials
+     * @param string $host
+     * @param int $port
+     * @param string $message
+     * @param array $options
+     * @return \Dragooon\Hawk\Client\Message
+     * @throws \InvalidArgumentException
+     */
+    public function createMessage(CredentialsInterface $credentials, $host, $port, $message, array $options = array());
+
 }
