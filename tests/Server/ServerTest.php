da7cf18101c102530a1bb79035c947bb16ea647c
Dragooon <mail@dragooon.net>
2015-06-05 14:30:48 +0530

    Add unit tests for creating server header

--- tests/Server/ServerTest.php
+++ tests/Server/ServerTest.php
@@ -6,6 +6,8 @@ use Dragooon\Hawk\Credentials\Credentials;
 use Dragooon\Hawk\Nonce\NonceProviderInterface;
 use Dragooon\Hawk\Time\ConstantTimeProvider;
 use Dragooon\Hawk\Time\TimeProviderInterface;
+use Dragooon\Hawk\Header\Header;
+use Dragooon\Hawk\Crypto\Artifacts;
 
 class ServerTest extends \PHPUnit_Framework_TestCase
 {
@@ -31,5 +33,77 @@ class ServerTest extends \PHPUnit_Framework_TestCase
         );
 
         $this->assertEquals('/posts', $response->artifacts()->resource());
+    }
+    /**
+     * @test
+     * @dataProvider headerDataProvider
+     *
+     * @param Credentials $credentials
+     * @param Artifacts $artifacts
+     * @param array $options
+     * @param mixed $expectedHeader False if the header is expected to throw an exception
+     * @param string $message
+     * @return void
+     */
+    public function shouldTestHeader(Credentials $credentials, Artifacts $artifacts, array $options, $expectedHeader, $message)
+    {
+        $server = ServerBuilder::create(
+            function($id) {
+                // We don't need this for testing header
+                return false;
+            }
+        )->build();
+
+        if ($expectedHeader === false) {
+            $this->setExpectedException('InvalidArgumentException');
+        }
+
+        $header = $server->createHeader($credentials, $artifacts, $options);
+
+        $this->assertEquals($expectedHeader, $header->fieldValue(), $message);
+    }
+
+    /**
+     * @return array
+     */
+    public function headerDataProvider()
+    {
+        return [
+            [
+                new Credentials('werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn', 'sha256', 123456),
+                new Artifacts('POST', 'example.com', 8080, '/resource/4?filter=a', 1398546787, 'xUwusx', 'some-app-data'),
+                ['payload' => 'some reply', 'content_type' => 'text/plain', 'ext' => 'response-specific'],
+                'Hawk mac="n14wVJK4cOxAytPUMc5bPezQzuJGl5n7MYXhFQgEKsE=", hash="f9cDF/TDm7TkYRLnGwRMfeDzT6LixQVLvrIKhh0vgmM=", ext="response-specific"',
+                'Should generate a valid header (with payload)',
+            ],
+            [
+                new Credentials('werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn', 'sha256', 123456),
+                new Artifacts('POST', 'example.com', 8080, '/resource/4?filter=a', 1398546787, 'xUwusx', 'some-app-data'),
+                ['payload' => '', 'content_type' => 'text/plain', 'ext' => 'response-specific'],
+                'Hawk mac="i8/kUBDx0QF+PpCtW860kkV/fa9dbwEoe/FpGUXowf0=", hash="q/t+NNAkQZNlq/aAD6PlexImwQTxwgT2MahfTa9XRLA=", ext="response-specific"',
+                'Should generate a valid header (without payload)',
+            ],
+            [
+                new Credentials('werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn', 'sha256', 123456),
+                new Artifacts('POST', 'example.com', 8080, '/resource/4?filter=a', 1398546787, 'xUwusx', 'some-app-data'),
+                ['payload' => 'some reply', 'content_type' => 'text/plain', 'ext' => null],
+                'Hawk mac="6PrybJTJs20jsgBw5eilXpcytD8kUbaIKNYXL+6g0ns=", hash="f9cDF/TDm7TkYRLnGwRMfeDzT6LixQVLvrIKhh0vgmM="',
+                'Should generate a valid header (without ext)',
+            ],
+            [
+                new Credentials('werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn', 'sha256'),
+                new Artifacts('POST', 'example.com', 8080, '/resource/4?filter=a', 1398546787, 'xUwusx', 'some-app-data'),
+                ['payload' => 'some reply', 'content_type' => 'text/plain', 'ext' => null],
+                'Hawk mac="6PrybJTJs20jsgBw5eilXpcytD8kUbaIKNYXL+6g0ns=", hash="f9cDF/TDm7TkYRLnGwRMfeDzT6LixQVLvrIKhh0vgmM="',
+                'Should generate generate a header (missing credentials ID)',
+            ],
+            [
+                new Credentials(null, 'sha256', 123456),
+                new Artifacts('POST', 'example.com', 8080, '/resource/4?filter=a', 1398546787, 'xUwusx', 'some-app-data'),
+                ['payload' => 'some reply', 'content_type' => 'text/plain', 'ext' => null],
+                false,
+                'Should throw an exception (missing credentials key)',
+            ],
+        ];
     }
 }
