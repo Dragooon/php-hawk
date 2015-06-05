818ca3312e69769eec1b46cbbfd76f4940d0fc80
Dragooon <mail@dragooon.net>
2015-06-05 14:22:37 +0530

    Convert arrays to short syntax
    Because why not, we're PHP 5.4+ anyways

--- tests/Client/ClientTest.php
+++ tests/Client/ClientTest.php
@@ -67,88 +67,88 @@ class ClientTest extends \PHPUnit_Framework_TestCase
      */
     public function headerDataProvider()
     {
-        return array(
-            array(
+        return [
+            [
                 new Credentials('2983d45yun89q', 'sha1', 123456),
                 'http://example.net/somewhere/over/the/rainbow',
                 'POST',
-                array('ext' => 'Bazinga!', 'timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => 'something to write about'),
+                ['ext' => 'Bazinga!', 'timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => 'something to write about'],
                 'Hawk id="123456", ts="1353809207", nonce="Ygvqdz", hash="bsvY3IfUllw6V5rvk4tStEvpBhE=", ext="Bazinga!", mac="qbf1ZPG/r/e06F4ht+T77LXi5vw="',
                 'Header with sha1 hash should be equal'
-            ),
-            array(
+            ],
+            [
                 new Credentials('2983d45yun89q', 'sha256', 123456),
                 'https://example.net/somewhere/over/the/rainbow',
                 'POST',
-                array('ext' => 'Bazinga!', 'timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => 'something to write about', 'content_type' => 'text/plain'),
+                ['ext' => 'Bazinga!', 'timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => 'something to write about', 'content_type' => 'text/plain'],
                 'Hawk id="123456", ts="1353809207", nonce="Ygvqdz", hash="2QfCt3GuY9HQnHWyWD3wX68ZOKbynqlfYmuO2ZBRqtY=", ext="Bazinga!", mac="q1CwFoSHzPZSkbIvl0oYlD+91rBUEvFk763nMjMndj8="',
                 'Header with sha256 hash should be equal'
-            ),
-            array(
+            ],
+            [
                 new Credentials('2983d45yun89q', 'sha256', 123456),
                 'https://example.net/somewhere/over/the/rainbow',
                 'POST',
-                array('timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => 'something to write about', 'content_type' => 'text/plain'),
+                ['timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => 'something to write about', 'content_type' => 'text/plain'],
                 'Hawk id="123456", ts="1353809207", nonce="Ygvqdz", hash="2QfCt3GuY9HQnHWyWD3wX68ZOKbynqlfYmuO2ZBRqtY=", mac="HTgtd0jPI6E4izx8e4OHdO36q00xFCU0FolNq3RiCYs="',
                 'Header with sha256 hash should be equal (with no ext)'
-            ),
-            array(
+            ],
+            [
                 new Credentials('2983d45yun89q', 'sha256', 123456),
                 'https://example.net/somewhere/over/the/rainbow',
                 'POST',
-                array('ext' => null, 'timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => 'something to write about', 'content_type' => 'text/plain'),
+                ['ext' => null, 'timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => 'something to write about', 'content_type' => 'text/plain'],
                 'Hawk id="123456", ts="1353809207", nonce="Ygvqdz", hash="2QfCt3GuY9HQnHWyWD3wX68ZOKbynqlfYmuO2ZBRqtY=", mac="HTgtd0jPI6E4izx8e4OHdO36q00xFCU0FolNq3RiCYs="',
                 'Header with sha256 hash should be equal (ext specified as null)'
-            ),
-            array(
+            ],
+            [
                 new Credentials('2983d45yun89q', 'sha256', 123456),
                 'https://example.net/somewhere/over/the/rainbow',
                 'POST',
-                array('timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => '', 'content_type' => 'text/plain'),
+                ['timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => '', 'content_type' => 'text/plain'],
                 'Hawk id="123456", ts="1353809207", nonce="Ygvqdz", hash="q/t+NNAkQZNlq/aAD6PlexImwQTxwgT2MahfTa9XRLA=", mac="U5k16YEzn3UnBHKeBzsDXn067Gu3R4YaY6xOt9PYRZM="',
                 'Header with sha256 hash should be equal (empty payload)'
-            ),
-            array(
+            ],
+            [
                 new Credentials('2983d45yun89q', 'sha256', 123456),
                 '',
                 'POST',
-                array('timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => '', 'content_type' => 'text/plain'),
+                ['timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => '', 'content_type' => 'text/plain'],
                 false,
                 'Header should return an error (missing URI)'
-            ),
-            array(
+            ],
+            [
                 new Credentials('2983d45yun89q', 'sha256', 123456),
                 4,
                 'POST',
-                array('timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => '', 'content_type' => 'text/plain'),
+                ['timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => '', 'content_type' => 'text/plain'],
                 false,
                 'Header should return an error (invalid URI)'
-            ),
-            array(
+            ],
+            [
                 new Credentials('2983d45yun89q', 'sha256', 123456),
                 'https://example.net/somewhere/over/the/rainbow',
                 '',
-                array('timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => '', 'content_type' => 'text/plain'),
+                ['timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => '', 'content_type' => 'text/plain'],
                 false,
                 'Header should return an error (missing method)'
-            ),
-            array(
+            ],
+            [
                 new Credentials('2983d45yun89q', 'sha256', 123456),
                 'https://example.net/somewhere/over/the/rainbow',
                 4,
-                array('timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => '', 'content_type' => 'text/plain'),
+                ['timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => '', 'content_type' => 'text/plain'],
                 false,
                 'Header should return an error (invalid method)'
-            ),
-            array(
+            ],
+            [
                 new Credentials('2983d45yun89q', 'sha256'),
                 'https://example.net/somewhere/over/the/rainbow',
                 'POST',
-                array('timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => '', 'content_type' => 'text/plain'),
+                ['timestamp' => 1353809207, 'nonce' => 'Ygvqdz', 'payload' => '', 'content_type' => 'text/plain'],
                 false,
                 'Header should return an error (invalid credentials)'
-            ),
-        );
+            ],
+        ];
     }
 
     /**
@@ -185,35 +185,35 @@ class ClientTest extends \PHPUnit_Framework_TestCase
      */
     public function messageDataProvider()
     {
-        return array(
-            array(
+        return [
+            [
                 new Credentials('2983d45yun89q', 'sha256', 123456),
                 'example.net',
                 80,
                 'I am the boodyman',
-                array('timestamp' => 1353809207, 'nonce' => 'abc123'),
+                ['timestamp' => 1353809207, 'nonce' => 'abc123'],
                 'fWpeQac+YUDgpFkOXiJCfHXV19FHU6uKJh2pXyKa8BQ=',
                 'Message authorization should be generated'
-            ),
-            array(
+            ],
+            [
                 new Credentials('2983d45yun89q', 'sha256'),
                 'example.net',
                 80,
                 'I am the boodyman',
-                array('timestamp' => 1353809207, 'nonce' => 'abc123'),
+                ['timestamp' => 1353809207, 'nonce' => 'abc123'],
                 false,
                 'Message authorization should fail on invalid credentials'
-            ),
-            array(
+            ],
+            [
                 new Credentials('2983d45yun89q', 'sha256', 123456),
                 '',
                 80,
                 'I am the boodyman',
-                array('timestamp' => 1353809207, 'nonce' => 'abc123'),
+                ['timestamp' => 1353809207, 'nonce' => 'abc123'],
                 false,
                 'Message authorization should fail on invalid host'
-            ),
-        );
+            ],
+        ];
     }
 
     /**
