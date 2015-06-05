371b3e7da14819fcd0fcebc7ca235c10034f6d3f
Dragooon <mail@dragooon.net>
2015-06-05 14:03:25 +0530

    Allow for empty content-type while creating server header

--- src/Server/Server.php
+++ src/Server/Server.php
@@ -170,16 +170,10 @@ class Server implements ServerInterface
      */
     public function createHeader(CredentialsInterface $credentials, Artifacts $artifacts, array $options = array())
     {
-        if (isset($options['payload']) || isset($options['content_type'])) {
-            if (isset($options['payload']) && isset($options['content_type'])) {
-                $payload = $options['payload'];
-                $contentType = $options['content_type'];
-                $hash = $this->crypto->calculatePayloadHash($payload, $credentials->algorithm(), $contentType);
-            } else {
-                throw new \InvalidArgumentException(
-                    "If one of 'payload' and 'content_type' are specified, both must be specified."
-                );
-            }
+        if (isset($options['payload'])) {
+            $payload = $options['payload'];
+            $contentType = !empty($options['content_type']) ? $options['content_type'] : '';
+            $hash = $this->crypto->calculatePayloadHash($payload, $credentials->algorithm(), $contentType);
         } else {
             $payload = null;
             $contentType = null;
