7cee07f52630d1a1f1de2cdb36df218c55c92756
Dragooon <mail@dragooon.net>
2015-06-05 17:55:50 +0530

    Add unit tests for server authentication

--- tests/Server/ServerTest.php
+++ tests/Server/ServerTest.php
@@ -8,6 +8,7 @@ use Dragooon\Hawk\Time\ConstantTimeProvider;
 use Dragooon\Hawk\Time\TimeProviderInterface;
 use Dragooon\Hawk\Header\Header;
 use Dragooon\Hawk\Crypto\Artifacts;
+use Dragooon\Hawk\Time\DefaultTimeProviderFactory;
 
 class ServerTest extends \PHPUnit_Framework_TestCase
 {
@@ -105,5 +106,257 @@ class ServerTest extends \PHPUnit_Framework_TestCase
                 'Should throw an exception (missing credentials key)',
             ],
         ];
+    }
+
+    /**
+     * @test
+     * @dataProvider authenticateDataProvider
+     *
+     * @param string $method
+     * @param string $host
+     * @param int $port
+     * @param string $resource
+     * @param string $contentType
+     * @param mixed $payload
+     * @param string $header
+     * @param int $localTimeOffsetSec
+     * @param mixed $expected true if a successful result is expected otherwise exception's message
+     * @param string $message
+     */
+    public function shouldTestAuthentication($method, $host, $port, $resource, $contentType, $payload, $header,
+                                             $localTimeOffsetSec, $expected, $message)
+    {
+        $key = 'werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn';
+
+        $serverBuilder = ServerBuilder::create(
+            function($id) use ($key) {
+                return new Credentials(
+                    $key,
+                    $id == 1 ? 'sha1' : 'sha256',
+                    $id
+                );
+            }
+        );
+
+        if (!empty($localTimeOffsetSec)) {
+            $serverBuilder->setLocaltimeOffsetSec($localTimeOffsetSec);
+        }
+
+        $server = $serverBuilder->build();
+
+        try {
+            $result = $server->authenticate($method, $host, $port, $resource, $contentType, $payload, $header);
+
+            if ($expected === true) {
+                $this->assertTrue($result instanceof Response, $message);
+                $this->assertEquals($key, $result->credentials()->key(), $message);
+            } else {
+                $this->fail($message);
+            }
+        } catch (\Exception $e) {
+            if (is_string($expected)) {
+                $this->assertEquals($expected, $e->getMessage(), $message);
+            } else {
+                $this->fail($message);
+            }
+        }
+
+    }
+
+    /**
+     * @return array
+     */
+    public function authenticateDataProvider()
+    {
+        $timeProvider = DefaultTimeProviderFactory::create();
+        $now = $timeProvider->createTimestamp();
+
+        return [
+            [
+                'GET',
+                'example.com',
+                8080,
+                '/resource/4?filter=a',
+                null,
+                null,
+                'Hawk id="1", ts="1353788437", nonce="k3j4h2", mac="zy79QQ5/EYFmQqutVnYb73gAc/U=", ext="hello"',
+                1353788437 - $now,
+                true,
+                'Should accept valid authentication request (GET with sha1)',
+            ],
+            [
+                'GET',
+                'example.com',
+                8000,
+                '/resource/1?b=1&a=2',
+                null,
+                null,
+                'Hawk id="dh37fgj492je", ts="1353832234", nonce="j4h3g2", mac="m8r1rHbXN6NgO+KIIhjO7sFRyd78RNGVUwehe8Cp2dU=", ext="some-app-data"',
+                1353832234 - $now,
+                true,
+                'Should accept valid authentication request (GET with sha256)',
+            ],
+            [
+                'POST',
+                'example.com',
+                8080,
+                '/resource/4?filter=a',
+                null,
+                null,
+                'Hawk id="123456", ts="1357926341", nonce="1AwuJD", hash="qAiXIVv+yjDATneWxZP2YCTa9aHRgQdnH9b3Wc+o3dg=", ext="some-app-data", mac="UeYcj5UoTVaAWXNvJfLVia7kU3VabxCqrccXP8sUGC4="',
+                1357926341 - $now,
+                true,
+                'Should accept valid authentication request (POST with sha256)',
+            ],
+            [
+                'POST',
+                'example.com',
+                8080,
+                '/resource/4?filter=a',
+                null,
+                'some message',
+                'Hawk id="123456", ts="1357926341", nonce="1AwuJD", hash="FF897AJ2LPnv/0ilMuEgXBWGImE+/9TuSfw1oi4Rsqk=", ext="some-app-data", mac="aMy5l6U7ePOyX0Kk2Aq3wEJhmKyWtiQqYBEgfymhKns="',
+                1357926341 - $now,
+                true,
+                'Should accept valid authentication request (POST with payload)',
+            ],
+            [
+                'POST',
+                'example.com',
+                8080,
+                '/resource/4?filter=a',
+                null,
+                'some message',
+                'Hawk id="123456", ts="1357926341", nonce="1AwuJD", hash="FF897AJ2LPnv/0ilMuEgXBWGImE+/9TuSfw1oi4Rsqk=", ext="some-app-data", mac="aMy5l6U7ePOyX0KtiQqYBEgfymhKns="',
+                1357926341 - $now,
+                'Bad MAC',
+                'Should reject for invalid Mac',
+            ],
+            [
+                'POST',
+                'example.com',
+                8080,
+                '/resource/4?filter=a',
+                null,
+                'some message',
+                'Hawk id="123456", ts="1357926341", nonce="1AwuJD", hash="qAiXIVv+yjDATneWxZP2YCTa9aHRgQdnH9b3Wc+o3dg=", ext="some-app-data", mac="UeYcj5UoTVaAWXNvJfLVia7kU3VabxCqrccXP8sUGC4="',
+                1357926341 - $now,
+                'Bad payload hash',
+                'Should reject for invalid hash',
+            ],
+            [
+                'POST',
+                'example.com',
+                8080,
+                '/resource/4?filter=a',
+                null,
+                'some message',
+                'Hawk id="123456", ts="1357926341", nonce="1AwuJD", ext="some-app-data", mac="XCqOLBuIUZQoNZzTtikW0v06zJhhDNGiKWNfuErWLJ4="',
+                1357926341 - $now,
+                'Missing required payload hash',
+                'Should reject for missing payload hash',
+            ],
+            [
+                'POST',
+                'example.com',
+                8080,
+                '/resource/4?filter=a',
+                null,
+                'some message',
+                'HawkUseless header',
+                1357926341 - $now,
+                'Missing attributes',
+                'Should reject for invalid header',
+            ],
+            [
+                'POST',
+                'example.com',
+                8080,
+                '/resource/4?filter=a',
+                null,
+                'some message',
+                'Hawk ts="1353788437", nonce="k3j4h2", mac="/qwS4UjfVWMcUyW6EEgUH4jlr7T/wuKe3dKijvTvSos=", ext="hello"',
+                1357926341 - $now,
+                'Missing attributes',
+                'Should reject for invalid header (missing ID)',
+            ],
+            [
+                'POST',
+                'example.com',
+                8080,
+                '/resource/4?filter=a',
+                null,
+                'some message',
+                'Hawk id="123", ts="1353788437", mac="/qwS4UjfVWMcUyW6EEgUH4jlr7T/wuKe3dKijvTvSos=", ext="hello"',
+                1357926341 - $now,
+                'Missing attributes',
+                'Should reject for invalid header (missing nonce)',
+            ],
+            [
+                'POST',
+                'example.com',
+                8080,
+                '/resource/4?filter=a',
+                null,
+                'some message',
+                'Hawk id="  ", ts="1353788437", nonce="k3j4h2", mac="/qwS4UjfVWMcUyW6EEgUH4jlr7T/wuKe3dKijvTvSos=", ext="hello"',
+                1357926341 - $now,
+                'Missing attributes',
+                'Should reject for invalid header (invalid ID)',
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     *
+     * Tests for replay nonce attack
+     */
+    public function shouldTestReplay()
+    {
+        $key = 'werxhqb98rpaxn39848xrunpaw3489ruxnpa98w4rxn';
+
+        $serverBuilder = ServerBuilder::create(
+            function($id) use ($key) {
+                return new Credentials(
+                    $key,
+                    $id == 1 ? 'sha1' : 'sha256',
+                    $id
+                );
+            }
+        );
+
+        $serverBuilder->setNonceValidator(
+            function($nonce, $timestamp) {
+                static $memory = array();
+                if (isset($memory[$nonce])) {
+                    throw new UnauthorizedException('Invalid nonce');
+                }
+                $memory[$nonce] = $timestamp;
+                return true;
+            }
+        );
+        $serverBuilder->setLocaltimeOffsetSec(1353788437 - time());
+
+        $server = $serverBuilder->build();
+
+        try {
+            for ($i = 0; $i < 2; $i++) {
+                $server->authenticate(
+                    'GET',
+                    'example.com',
+                    8080,
+                    '/resource/4?filter=a',
+                    null,
+                    null,
+                    'Hawk id="1", ts="1353788437", nonce="k3j4h2", mac="zy79QQ5/EYFmQqutVnYb73gAc/U=", ext="hello"'
+                );
+            }
+
+            $this->fail('Should reject duplicate nonce');
+        }
+        catch (UnauthorizedException $e) {
+            $this->assertEquals('Invalid nonce', $e->getMessage());
+        }
     }
 }
