470fc2e77585939ca4fb3bf041533e3b8c5bc784
Dragooon <mail@dragooon.net>
2015-06-07 04:15:49 +0530

    Add tests for HeaderFactory and HeaderParser

---
+++ tests/Header/HeaderFactoryTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Dragooon\Hawk\Header;
+
+class HeaderFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldTestObjectOrString()
+    {
+        $header = 'Hawk id="123456", ts="1353809207", nonce="Ygvqdz", hash="bsvY3IfUllw6V5rvk4tStEvpBhE=", ext="Bazinga!", mac="qbf1ZPG/r/e06F4ht+T77LXi5vw="';
+        $header = HeaderFactory::createFromHeaderObjectOrString(
+            'Test',
+            $header,
+            function() {
+                return true;
+            }
+        );
+
+        $this->assertTrue($header instanceof Header);
+        $this->assertEquals(123456, $header->attribute('id'));
+        $this->assertEquals('Ygvqdz', $header->attribute('nonce'));
+
+        try {
+            $header = 'InvalidHawkHeader';
+            HeaderFactory::createFromHeaderObjectOrString(
+                'Test',
+                $header,
+                function () {
+                }
+            );
+            $this->fail('Should throw an exception for invalid header');
+        } catch (NotHawkAuthorizationException $e) {
+        }
+
+        $header = HeaderFactory::createFromHeaderObjectOrString(
+            'Test',
+            new Header('Test', 'Hawk id="123456"', array('id' => 123456)),
+            function() {
+            }
+        );
+        $this->assertTrue($header instanceof Header);
+        $this->assertEquals(123456, $header->attribute('id'));
+    }
+
+
+    /**
+     * @test
+     */
+    public function shouldTestString()
+    {
+        $header = 'Hawk id="123456", ts="1353809207", nonce="Ygvqdz", hash="bsvY3IfUllw6V5rvk4tStEvpBhE=", ext="Bazinga!", mac="qbf1ZPG/r/e06F4ht+T77LXi5vw="';
+        $header = HeaderFactory::createFromString('Test', $header);
+
+        $this->assertTrue($header instanceof Header);
+        $this->assertEquals(123456, $header->attribute('id'));
+        $this->assertEquals('Ygvqdz', $header->attribute('nonce'));
+    }
+}
--- tests/Header/HeaderParserTest.php
+++ tests/Header/HeaderParserTest.php
@@ -1,7 +1,56 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: shitiz
- * Date: 07/06/15
- * Time: 3:40 AM
- */
+
+namespace Dragooon\Hawk\Header;
+
+class HeaderParserTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldTestNotHawkAuthorizationException()
+    {
+        try {
+            HeaderParser::parseFieldValue('TestHeader mac="test"');
+            $this->fail('Should throw an exception for invalid header');
+        } catch (NotHawkAuthorizationException $e) {
+        }
+
+        return true;
+    }
+
+    /**
+     * @test
+     */
+    public function shouldTestFieldValueParserException()
+    {
+        try {
+            $header = 'Hawk id="123456", ts="1353809207", hash="bsvY3IfUllw6V5rvk4tStEvpBhE=", ext="Bazinga!", mac="qbf1ZPG/r/e06F4ht+T77LXi5vw="';
+            HeaderParser::parseFieldValue($header, array('id', 'nonce', 'hash'));
+            $this->fail('Should throw an exception for missing required values');
+        } catch (FieldValueParserException $e) {
+        }
+
+        return true;
+    }
+
+    /**
+     * @test
+     */
+    public function shouldTestParseFieldValue()
+    {
+        $header = 'Hawk id="123456", ts="1353809207", nonce="Ygvqdz", hash="bsvY3IfUllw6V5rvk4tStEvpBhE=", ext="Bazinga!", mac="qbf1ZPG/r/e06F4ht+T77LXi5vw="';
+        $header = HeaderParser::parseFieldValue($header, array('id', 'ts'));
+
+        $this->assertTrue(is_array($header));
+        $this->assertEquals(123456, $header['id']);
+        $this->assertEquals(1353809207, $header['ts']);
+
+        try {
+            $header = 'Hawk wrong="test"';
+            HeaderParser::parseFieldValue($header);
+            $this->fail('Should throw an exception for invalid key');
+        } catch (FieldValueParserException $e) {
+            $this->assertEquals('Invalid key: wrong', $e->getMessage());
+        }
+    }
+}
