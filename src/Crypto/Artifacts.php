abae987dcf91f167a5d19e92ce92076f9acfd62e
Dragooon <mail@dragooon.net>
2015-06-07 01:37:18 +0530

    Complete phpDocs for \Dragooon\Hawk\Crypto\Artifacts

--- src/Crypto/Artifacts.php
+++ src/Crypto/Artifacts.php
@@ -17,6 +17,20 @@ class Artifacts
     private $app;
     private $dlg;
 
+    /**
+     * @param string $method
+     * @param string $host
+     * @param int $port
+     * @param string $resource
+     * @param int $timestamp
+     * @param string $nonce
+     * @param string $ext
+     * @param string $payload
+     * @param string $contentType
+     * @param string $hash
+     * @param string $app
+     * @param string $dlg
+     */
     public function __construct(
         $method,
         $host,
@@ -45,61 +59,97 @@ class Artifacts
         $this->dlg = $dlg;
     }
 
+    /**
+     * @return int
+     */
     public function timestamp()
     {
         return $this->timestamp;
     }
 
+    /**
+     * @return string
+     */
     public function nonce()
     {
         return $this->nonce;
     }
 
+    /**
+     * @return null|string
+     */
     public function ext()
     {
         return $this->ext;
     }
 
+    /**
+     * @return null|string
+     */
     public function payload()
     {
         return $this->payload;
     }
 
+    /**
+     * @return null|string
+     */
     public function contentType()
     {
         return $this->contentType;
     }
 
+    /**
+     * @return null|string
+     */
     public function hash()
     {
         return $this->hash;
     }
 
+    /**
+     * @return null|string
+     */
     public function app()
     {
         return $this->app;
     }
 
+    /**
+     * @return null|string
+     */
     public function dlg()
     {
         return $this->dlg;
     }
 
+    /**
+     * @return string
+     */
     public function resource()
     {
         return $this->resource;
     }
 
+    /**
+     * @return string
+     */
     public function host()
     {
         return $this->host;
     }
 
+    /**
+     * @return int
+     */
     public function port()
     {
         return $this->port;
     }
 
+    /**
+     * @return string
+     */
     public function method()
     {
         return $this->method;
